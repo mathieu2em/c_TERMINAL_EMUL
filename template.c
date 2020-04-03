974d0fe63b2fd71633714ffa54503e0a5653f452
Amine Sami <amine.sami.1997@gmail.com>
2020-04-03 17:09:20 -0400

    sque j'ai

--- template.c
+++ template.c
@@ -371,6 +371,7 @@ error_code parse (char **tokens, command_head *cmd_head) {
                     if (cp = strchr(tokens[i], ')')) {
                         *cp = '\0';
                         rnfn = false;
+                        op = NONE;
                     }
                 }
                 /* j = i;*/
@@ -383,8 +384,9 @@ error_code parse (char **tokens, command_head *cmd_head) {
         }
 
         if (op != BIDON) {
-            if (op != NONE)
+            if (op != NONE) {
                 tokens[i] = NULL;
+            }
             cmd = make_command_node(tokens + j, op, count, NULL);
             if (!cmd)
                 goto parse_error;
@@ -1027,6 +1029,8 @@ void call_bankers(banker_customer *customer) {
             customer->current_resources[i] += c->ressources[i];
 
         current->depth = -1; // grant request
+        pthread_mutex_unlock(current->head->mutex);
+        //printf("released customer's mutex\n");
     } else {
         // otherwise reset _available to original state
         for(i = 0; i < n; i++){
@@ -1034,8 +1038,6 @@ void call_bankers(banker_customer *customer) {
         }
     }
 
-    pthread_mutex_unlock(current->head->mutex);
-    //printf("released customer's mutex\n");
     // liberate the mutex
     pthread_mutex_unlock(available_mutex);
     //printf("released available mutex\n");
@@ -1058,7 +1060,7 @@ void *banker_thread_run() {
         //printf("banker_thread...\n");
         while (current) {
             // 3. En trouver un dont le depth n'est pas -1
-            pthread_mutex_lock(current->head->mutex);
+            pthread_mutex_trylock(current->head->mutex);
             //printf("got customer's mutex\n");
             if (current->depth >= 0) {
                 // 4. Appelle call_bankers sur ce client
@@ -1093,7 +1095,7 @@ error_code request_resource(banker_customer *customer, int cmd_depth) {
 
     printf("request resource for '%s %s' (bg : %s)...\n",
            customer->head->command->call[0],
-           customer->head->command->call[1],
+           customer->head->command->call_size > 1 ? customer->head->command->call[1] : "",
            customer->head->background ? "true" : "false");
     pthread_mutex_lock(register_mutex);
     customer->depth = cmd_depth;
@@ -1101,22 +1103,23 @@ error_code request_resource(banker_customer *customer, int cmd_depth) {
 
     //let banker_thread lock mutex
     //pthread_mutex_lock(register_mutex);
-    sleep(1);
+    //sleep(1);
+    //pthread_mutex_unlock(register_mutex);
     // wait for autorisation
     //printf("waiting for autorisation...\n");
+    printf("waiting for '%s %s' (bg : %s)...\n",
+           customer->head->command->call[0],
+           customer->head->command->call_size > 1 ? customer->head->command->call[1] : "",
+           customer->head->background ? "true" : "false");
     pthread_mutex_lock(customer->head->mutex);
     //printf("receiving anwser...\n");
-    printf("receiving anwser for '%s %s' (bg : %s)...\n",
+    printf("permission granted for '%s %s' (bg : %s)...\n",
            customer->head->command->call[0],
-           customer->head->command->call[1],
+           customer->head->command->call_size > 1 ? customer->head->command->call[1] : "",
            customer->head->background ? "true" : "false");
-    ret = customer->depth < 0 ? NO_ERROR : -1;
-    customer->depth = -1;
     pthread_mutex_unlock(customer->head->mutex);
 
-    //pthread_mutex_unlock(register_mutex);
-
-    return ret;
+    return NO_ERROR;
 }
 
 // cette fonction va avoir une loop qui va call request ressources pour tt la commande
@@ -1135,8 +1138,7 @@ void *command_handler(void *arg){
     // dependamment de cque exec command retourne et du type dla commande
     // ca setup les shit pour la prochaine iteration de la boucle
     while (current && current->call) { // TODO on checkera si le deuxieme cond est necessaire lol
-        if(HAS_ERROR(request_resource(c, depth)))
-            continue;
+        request_resource(c, depth);
 
         //printf("executing '%s' (depth: %d)...\n", current->call[0], depth);
         ret = execute_cmd(current);
